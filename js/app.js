5f2a422178df15a88a297e8ea9d8f8f2576f5db6
banusion <banusion69@free.fr>
2017-03-21 23:06:45 +0100

    Update mineur

--- js/app.js
+++ js/app.js
@@ -2,6 +2,16 @@ new Vue({
 	el: '#app',
 	data: {
 		message: 'Salut les gens',
-		link: 'http//www.homeserve.fr'
+		link: 'http//www.homeserve.fr',
+		yo: 'ça marche ;) ',
+		success: true,
+		persons: ['Manu', 'Cynthia', 'Mina', 'Elena', 'Luna']
+	}, 
+
+	methods: {
+		close:function() {
+			this.success = false
+			this.message = "Fermé"
+		}
 	}
 })
