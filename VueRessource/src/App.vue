3b5057cf5b6993153f61cde158307c5379374c10
banusion <banusion69@free.fr>
2017-05-25 21:05:20 +0200

    gestion API avec modify/delete + loading

--- VueRessource/src/App.vue
+++ VueRessource/src/App.vue
@@ -1,29 +1,74 @@
 <template>
   <div id="app">
-    {{ users }}
+  <div class="ui active dimmer" v-if="loading">
+    <div class="ui loader"></div>
+  </div>
+    <div class="ui four column grid">
+      <div class="column" v-for="user in users">
+        <div class="ui card">
+          <div class="content">
+            <div class="header">
+              <input type="text" v-model="user.name" style="widht:100%">
+            </div>
+            <div class="meta">
+              <a>Friend</a>
+            </div>
+            <div class="description">
+              {{ user.address.street }}<br>
+              {{ user.address.zipcode }} {{ user.address.city }} <br>
+            </div>
+          </div>
+          <div class="extra content">
+            <div class="ui two buttons">
+              <div class="ui basic green button" @click="save(user)">Modifier</div>
+              <div class="ui basic red button" @click="destroy(user)">Supprimer</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
   data () {
     return {
-      users: []
+      users: [],
+      loading: false
+    }
+  },
+  mounted () {
+    this.$http.get('https://jsonplaceholder.typicode.com/users').then((response) => {
+      this.users = response.data
+    }, (response) => {
+      console.log('error', response)
+    })
+  },
+  methods: {
+    save (user) {
+      this.loading = true
+      this.$http.put('https://jsonplaceholder.typicode.com/users/' + user.id, {
+        name: user.name
+      }).then((response) => {
+      }, (response) => {
+        console.log('error', response)
+      }).then(_ => {
+        this.loading = false
+      })
+    },
+    destroy (user) {
+      this.loading = true
+      this.$http.delete('https://jsonplaceholder.typicode.com/users/' + user.id)
+      .then((response) => {
+        this.users = this.users.filter(u => u !== user)
+      }, (response) => {
+        console.log('error', response)
+      }).then(_ => {
+        this.loading = false
+      })
     }
   }
-
 }
-
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
